524dbe7e4ccff18f536565a6be703f18dbf5257c
Fabio Dela Bruna <fabiodelabruna@gmail.com>
2018-05-27 19:38:08 -0300

    Added Bean Validation

--- src/main/java/com/example/restapi/demo/resource/CategoryResource.java
+++ src/main/java/com/example/restapi/demo/resource/CategoryResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -32,7 +33,7 @@ public class CategoryResource {
     }
 
     @PostMapping
-    public ResponseEntity<Category> create(@RequestBody Category category) {
+    public ResponseEntity<Category> create(@Valid @RequestBody Category category) {
         Category storedCategory = categoryRepository.save(category);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
