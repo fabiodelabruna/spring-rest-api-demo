195f40e7778a378b53e2733c0e3decfe24393cc9
Fabio Dela Bruna <fabiodelabruna@gmail.com>
2018-05-28 13:37:43 -0300

    Create entry resource save action

--- src/main/java/com/example/restapi/demo/resource/EntryResource.java
+++ src/main/java/com/example/restapi/demo/resource/EntryResource.java
@@ -1,14 +1,16 @@
 package com.example.restapi.demo.resource;
 
+import com.example.restapi.demo.event.CreatedResourceEvent;
 import com.example.restapi.demo.model.Entry;
 import com.example.restapi.demo.repository.EntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +21,9 @@ public class EntryResource {
     @Autowired
     private EntryRepository entryRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Entry> findAll() {
         return entryRepository.findAll();
@@ -28,6 +33,13 @@ public class EntryResource {
     public ResponseEntity<?> findById(@PathVariable Long id) {
         Optional<Entry> entry = entryRepository.findById(id);
         return entry.isPresent() ? ResponseEntity.ok(entry.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Entry> create(@Valid @RequestBody Entry entry, HttpServletResponse response) {
+        Entry storedEntry = entryRepository.save(entry);
+        publisher.publishEvent(new CreatedResourceEvent(this, response, storedEntry.getId()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(storedEntry);
     }
 
 }
